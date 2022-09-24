f7e2001123a572e48d5757825fe0fbc0dfa8a973
Lauren Hoock <laurenh8916@protonmail.com>
2022-09-24 15:49:44 -0600

    subevent results relation added

--- app/models/subevent.rb
+++ app/models/subevent.rb
@@ -1,7 +1,7 @@
 class Subevent < ApplicationRecord
   belongs_to :event 
-  has_many :results 
   has_many :team_entries
+  has_many :results, through: :team_entries 
   has_many :competitors, through: :team_entries 
   has_many :teams, through: :team_entries 
 end
