683faca13611a809c6ea8384027346f36f6a0231
Lauren Hoock <laurenh8916@protonmail.com>
2022-09-24 03:48:26 -0600

    team model update

--- app/models/team.rb
+++ app/models/team.rb
@@ -1,8 +1,7 @@
 class Team < ApplicationRecord
   belongs_to :nationality 
-  has_many :competitors 
-  has_many :vehicles 
   has_many :event_entries 
   has_many :competitors, through: :event_entries 
   has_many :events, through: :event_entries 
+  has_many :vehicles, through: :event_entries 
 end
