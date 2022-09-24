0d402bbd27f8e17bfc80eb75670ee499d54dc0c5
Lauren Hoock <laurenh8916@protonmail.com>
2022-09-24 17:02:15 -0600

    app controllers crud wip

--- app/controllers/competitors_controller.rb
+++ app/controllers/competitors_controller.rb
@@ -1,11 +1,45 @@
 class CompetitorsController < ApplicationController
+  #wrap_parameters format: []
+rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
+
   def index 
     competitors = Competitor.all 
     render json: competitors 
   end
 
   def show 
-    competitor = Competitor.find(params[:id])
+    competitor = find_competitor 
     render json: competitor 
+  end
+
+  def create 
+    competitor = Competitor.create(competitor_params)
+    render json: competitor, status: :created 
+  end
+
+  def update 
+    competitor = find_competitor 
+    competitor.update(competitor_params)
+    render json: competitor 
+  end
+
+  def destroy 
+    competitor = find_competitor 
+    competitor.destroy 
+    head :no_content 
+  end
+
+  private 
+
+  def competitor_params 
+    params.permit(:first_name, :last_name, :nationality_id)
+  end
+
+  def find_competitor
+    Competitor.find_by(id: params[:id])
+  end
+
+  def render_not_found_response
+    render json: { error: "Competitor not found" }, status: :not_found
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,21 +1,28 @@
 Rails.application.routes.draw do
 
-  get '/series', to: 'serieses#index'
-  get '/series/:id', to: 'serieses#show'
+  # get '/series', to: 'serieses#index'
+  # get '/series/:id', to: 'serieses#show'
 
-  get '/competitors', to: 'competitors#index'
-  get '/competitors/:id', to: 'competitors#show'
+  # get '/competitors', to: 'competitors#index'
+  # get '/competitors/:id', to: 'competitors#show'
 
-  get '/nationalities', to: 'nationalities#index'
-  get '/nationalities/:id', to: 'nationalities#show'
+  # get '/nationalities', to: 'nationalities#index'
+  # get '/nationalities/:id', to: 'nationalities#show'
 
-  get '/manufacturers', to: 'manufacturers#index'
-  get '/manufacturers/:id', to: 'manufacturers#show'
+  # get '/manufacturers', to: 'manufacturers#index'
+  # get '/manufacturers/:id', to: 'manufacturers#show'
 
-  get '/teams', to: 'teams#index'
-  get '/teams/:id', to: 'teams#show'
+  # get '/teams', to: 'teams#index'
+  # get '/teams/:id', to: 'teams#show'
 
-  get '/tracks', to: 'tracks#index'
-  get '/tracks/:id', to: 'tracks#show'
+  # get '/tracks', to: 'tracks#index'
+  # get '/tracks/:id', to: 'tracks#show'
+
+  resources :serieses, only: [:index, :show]
+  resources :competitors, only: [:index, :show, :create, :update, :destroy]
+  resources :nationalities, only: [:index, :show]
+  resources :manufacturers, only: [:index, :show]
+  resources :teams, only: [:index, :show]
+  resources :tracks, only: [:index, :show]
 
 end
