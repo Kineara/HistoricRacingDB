355c3917ce413809f6370acf37c3b26100da7811
Lauren Hoock <laurenh8916@protonmail.com>
2022-09-24 17:13:53 -0600

    routes update

--- config/routes.rb
+++ config/routes.rb
@@ -1,28 +1,10 @@
 Rails.application.routes.draw do
 
-  # get '/series', to: 'serieses#index'
-  # get '/series/:id', to: 'serieses#show'
-
-  # get '/competitors', to: 'competitors#index'
-  # get '/competitors/:id', to: 'competitors#show'
-
-  # get '/nationalities', to: 'nationalities#index'
-  # get '/nationalities/:id', to: 'nationalities#show'
-
-  # get '/manufacturers', to: 'manufacturers#index'
-  # get '/manufacturers/:id', to: 'manufacturers#show'
-
-  # get '/teams', to: 'teams#index'
-  # get '/teams/:id', to: 'teams#show'
-
-  # get '/tracks', to: 'tracks#index'
-  # get '/tracks/:id', to: 'tracks#show'
-
-  resources :serieses, only: [:index, :show]
+  resources :serieses
   resources :competitors, only: [:index, :show, :create, :update, :destroy]
-  resources :nationalities, only: [:index, :show]
-  resources :manufacturers, only: [:index, :show]
-  resources :teams, only: [:index, :show]
-  resources :tracks, only: [:index, :show]
+  resources :nationalities
+  resources :manufacturers
+  resources :teams
+  resources :tracks
 
 end
