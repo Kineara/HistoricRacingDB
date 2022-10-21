340217daaf9009cb8a4cdf090c5d0a2fc5d207b9
Lauren Hoock <laurenh8916@protonmail.com>
2022-10-21 01:06:42 -0600

    table edits

--- client/src/components/Database/DataTable.js
+++ client/src/components/Database/DataTable.js
@@ -7,18 +7,7 @@ import TableHead from "@mui/material/TableHead";
 import TableRow from "@mui/material/TableRow";
 import Paper from "@mui/material/Paper";
 import { useSelector, useDispatch } from "react-redux";
-
-function createData(name, calories, fat, carbs, protein) {
-  return { name, calories, fat, carbs, protein };
-}
-
-const rows = [
-  createData("Frozen yoghurt", 159, 6.0, 24, 4.0),
-  createData("Ice cream sandwich", 237, 9.0, 37, 4.3),
-  createData("Eclair", 262, 16.0, 24, 6.0),
-  createData("Cupcake", 305, 3.7, 67, 4.3),
-  createData("Gingerbread", 356, 16.0, 49, 3.9),
-];
+import Typography from "@mui/material/Typography";
 
 export default function BasicTable({ data }) {
   // console.log(Object.keys(data));
@@ -43,7 +32,23 @@ export default function BasicTable({ data }) {
       <TableRow>
         {tableData.map((entry) => {
           if (typeof entry === "object") {
-            return <TableCell>Nested data</TableCell>;
+            return (
+            <TableCell>
+              <Table>
+                <TableHead>{createHeadings(entry)}</TableHead>
+                <TableBody>
+                  <TableRow>
+                  {Object.values(entry).map((recEntry) => {
+                    return (
+                      <TableCell>
+                        {recEntry}
+                      </TableCell>
+                    )
+                  })}
+                  </TableRow>
+                </TableBody>
+              </Table>
+            </TableCell>);
           } else {
             return <TableCell>{entry}</TableCell>;
           }
@@ -52,30 +57,18 @@ export default function BasicTable({ data }) {
     );
   }
 
-  createValues(data);
-
+  if (data) {
   return (
     <TableContainer component={Paper}>
       <Table sx={{ minWidth: 650 }} aria-label="simple table">
         <TableHead>{createHeadings(data)}</TableHead>
         <TableBody>
           {createValues(data)}
-          {/* {rows.map((row) => (
-            <TableRow
-              key={row.name}
-              sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
-            >
-              <TableCell component="th" scope="row">
-                {row.name}
-              </TableCell>
-              <TableCell align="right">{row.calories}</TableCell>
-              <TableCell align="right">{row.fat}</TableCell>
-              <TableCell align="right">{row.carbs}</TableCell>
-              <TableCell align="right">{row.protein}</TableCell>
-            </TableRow>
-          ))} */}
         </TableBody>
       </Table>
-    </TableContainer>
-  );
+    </TableContainer> 
+    );
+  } else {
+    return <Typography>Table Data Loading...</Typography>
+  }
 }
