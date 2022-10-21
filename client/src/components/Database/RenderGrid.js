099fa0aa1cc9f187e62266a6635215c10df2d633
Lauren Hoock <laurenh8916@protonmail.com>
2022-10-20 19:12:44 -0600

    heroku edits

--- client/src/components/Database/RenderGrid.js
+++ client/src/components/Database/RenderGrid.js
@@ -1,17 +1,23 @@
 import React, { useState } from "react";
 import { Grid } from "gridjs-react";
 
+// function RenderGrid() {
+//   const row = () => ["riley", "hoock"];
+//   const [data, setData] = useState([row()]);
+//   const update = () => {
+//     setData(data.slice(0).concat([row()]));
+//   }
+//   return (
+//     <div>      
+//       <Grid data={data} />
+//     </div>
+//   );
+// }
+
 function RenderGrid() {
-  const row = () => ["riley", "hoock"];
-  const [data, setData] = useState([row()]);
-  const update = () => {
-    setData(data.slice(0).concat([row()]));
-  }
   return (
-    <div>      
-      <Grid data={data} />
-    </div>
-  );
+    <div>Grid</div>
+  )
 }
 
 export default RenderGrid;
--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,5 @@
 Rails.application.routes.draw do
-
   namespace :api do 
-    # resources :serieses, only: [:index, :show]
-    # resources :seasons, only: [:show]
-    # resources :championships, only: [:show]
-    # resources :events, only: [:index, :show]
-    # resources :competitors, only: [:index, :show, :create, :update, :destroy]
-    # resources :nationalities
-    # resources :manufacturers
-    # resources :teams
-    # resources :tracks
-    # resources :db_summary, only: [:index]
-
     namespace :v1 do 
       resources :users, only: [:create]
       post '/login', to: 'auth#create'
